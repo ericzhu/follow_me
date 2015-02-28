df846931b468f6a665e99db13061fd9267af9512
Xiao Yu Zhu <cj_zhuyu@yahoo.com>
2015-02-27 20:17:29 -0500

    Create full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#return a full title on a per-page base
+	def full_title(page_title = "")
+		base_title = "Follow Me"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
